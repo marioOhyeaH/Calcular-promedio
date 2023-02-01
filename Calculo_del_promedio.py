6934acf83d59a5c774d70947b06859d1aeed3f06
marioOhyeaH <89437119+marioOhyeaH@users.noreply.github.com>
2023-02-01 18:04:19 -0500

    Add files via upload

---
+++ Calculo_del_promedio.py
@@ -0,0 +1,15 @@
+# -*- coding: iso-8859-15 -*-
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+    print("Nota 1:")
+    nota1 = float(input())
+    print("Nota 2:")
+    nota2 = float(input())
+    print("Nota 3:")
+    nota3 = float(input())
+    promedio = (nota1 + nota2 + nota3) / 3
+    print("Tu promedio es:", promedio)
